257db82f191c44bb526d3279a989856575664299
Anas Mohamed Elsebay Mohamed <anas.moelsebay@gmail.com>
2024-09-06 22:22:09 +0800

    seeder

--- main.go
+++ main.go
@@ -3,40 +3,14 @@ package main
 import (
 	"context"
 	"fmt"
-	"net/url"
 	"os"
 	"strconv"
+	"task-scheduler/database"
 	"task-scheduler/invoker"
 	"task-scheduler/server"
 
-	"github.com/jackc/pgx/v5"
 	"github.com/joho/godotenv"
 )
-
-func dbConnect(ctx context.Context) (*pgx.Conn, error) {
-	dsn := url.URL{
-		Scheme: "postgres",
-		Host:   os.Getenv("DATABASE_HOST") + ":" + os.Getenv("DATABASE_PORT"),
-		User: url.UserPassword(
-			os.Getenv("POSTGRES_USER"),
-			os.Getenv("POSTGRES_PASSWORD"),
-		),
-		Path: os.Getenv("DATABASE_NAME"),
-	}
-
-	q := dsn.Query()
-	q.Add("sslmode", "disable")
-	dsn.RawQuery = q.Encode()
-
-	println(dsn.String())
-
-	conn, err := pgx.Connect(ctx, dsn.String())
-	if err != nil {
-		return nil, err
-	}
-
-	return conn, nil
-}
 
 func main() {
 	ctx := context.Background()
@@ -45,7 +19,7 @@ func main() {
 		panic(err)
 	}
 
-	conn, err := dbConnect(ctx)
+	conn, err := database.DbConnect(ctx)
 	if err != nil {
 		panic(err)
 	}
